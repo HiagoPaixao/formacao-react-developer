9ef6d6d76de4187c448bc99967b07b3740b26046
Hiago Gonçalves <hiago@golerp.onmicrosoft.com>
2025-04-22 16:58:26 -0300

    Criando os botões principais

--- Fundamentos React/Criando projeto REACT/desafio01-calculadora/src/App.js
+++ Fundamentos React/Criando projeto REACT/desafio01-calculadora/src/App.js
@@ -2,7 +2,7 @@
 import Input from './components/Input'
 import Button from './components/Button'
 
-import {Container, Content} from './styles'
+import {Container, Content, Row, Column} from './styles'
 
 const App = () => {
   return (
@@ -10,6 +10,24 @@ const App = () => {
       <Content>
         <Input>        
         </Input>
+        <Row>
+          <Button label="1" onClick={1}/>
+          <Button label="2" onClick={1}/>
+          <Button label="3" onClick={1}/>
+          <Button label="-" onClick={1}/>
+        </Row>
+        <Row>
+          <Button label="4" onClick={1}/>
+          <Button label="5" onClick={1}/>
+          <Button label="6" onClick={1}/>
+          <Button label="+" onClick={1}/>
+        </Row>
+        <Row>
+          <Button label="1" onClick={1}/>
+          <Button label="2" onClick={1}/>
+          <Button label="3" onClick={1}/>
+          <Button label="=" onClick={1}/>
+        </Row>
 
       </Content>
     </Container>
--- Fundamentos React/Criando projeto REACT/desafio01-calculadora/src/components/Button/styles.js
+++ Fundamentos React/Criando projeto REACT/desafio01-calculadora/src/components/Button/styles.js
@@ -1,2 +1,11 @@
 import styled from 'styled-components';
 
+export const ButtonContainer = styled.button`
+    padding: 10px;
+    border: 1px solid #ccc;
+    background-color: #f0f0f0;
+    color: #333;
+    font-size: 24px;
+    font-weight: bold;
+    flex: 1;
+    `
