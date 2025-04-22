e4cd849dc576aafd929bacf65c9c3ea9ea3adcee
Hiago Gonçalves <hiago@golerp.onmicrosoft.com>
2025-04-22 16:30:39 -0300

    Criação do componente Input

--- Fundamentos React/Criando projeto REACT/desafio01-calculadora/src/components/Input/styles.js
+++ Fundamentos React/Criando projeto REACT/desafio01-calculadora/src/components/Input/styles.js
@@ -1,2 +1,20 @@
 import styled from 'styled-components';
 
+export const InputContainer = styled.div`
+width: 100%;
+height: 75%;
+background-color: #aaffaa;
+display: flex;
+align-items: center;
+justify-content: flex-end;
+font-size: 24px;
+
+input {
+    width: 100%;
+    height: 75px;
+    background-color: #aaffaa;
+    border: none;
+    padding: 0 10px;
+    font-size: 24px;
+    }
+`
