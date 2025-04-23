37ebe2ce47d84b032e0a9c334c4109a13d3a4431
Hiago Gonçalves <hiago@golerp.onmicrosoft.com>
2025-04-23 11:48:04 -0300

    Criacao de funções de somar, subtrair e igual

--- Fundamentos React/Criando projeto REACT/desafio01-calculadora/src/App.js
+++ Fundamentos React/Criando projeto REACT/desafio01-calculadora/src/App.js
@@ -9,20 +9,58 @@ import { useEffect } from 'react';
 
 
 const App = () => {
-  const [currentValue, setCurrentValue] = useState("0");
+  const [currentValue, setCurrentValue] = useState("");
   const [firstNumber, setFirstNumber] = useState(null);
-  const [secondNumber, setSecondNumber] = useState(null);
   const [operation, setOperation] = useState(null);
   const [result, setResult] = useState(null);
 
   const handleClear = () => {
-    setCurrentValue("0");
+    setCurrentValue("");
   }
 
   const handleAddValue = (value) => {
-    setCurrentValue(prev => `${value}${prev}`);
+    setCurrentValue(prev => `${prev}${value}`);
   }
 
+  const handleSumNumbers = () => {
+    if (firstNumber === null) {
+      setFirstNumber(currentValue);
+      setOperation('+');
+      setCurrentValue("");
+      handleClear();
+    }else {
+      const sum = Number(firstNumber) + Number(currentValue);
+      setCurrentValue(String(sum));
+    }
+  }
+
+  const handleSubNumbers = () => {
+    if (firstNumber === null) {
+      setFirstNumber(currentValue);
+      setOperation('-');
+      setCurrentValue("");
+      handleClear();
+    }else {
+      const sum = Number(firstNumber) - Number(currentValue);
+      setCurrentValue(String(sum));
+    }
+  }
+
+  const handleEquals = () => {
+    if (operation === '+') {
+      const sum = Number(firstNumber) + Number(currentValue);
+      setCurrentValue(String(sum));
+    } else if (operation === '-') {
+      const sub = Number(firstNumber) - Number(currentValue);
+      setCurrentValue(String(sub));
+    } else if (operation === '*') {
+      const mult = Number(firstNumber) * Number(currentValue);
+      setCurrentValue(String(mult));
+    } else if (operation === '/') {
+      const div = Number(firstNumber) / Number(currentValue);
+      setCurrentValue(String(div));
+    }
+  }
 
   return (
     <Container>
@@ -30,22 +68,22 @@ const App = () => {
         <Input value={currentValue}>        
         </Input>
         <Row>
-          <Button label="C" onClick={()=>handleClear()}/>
+          <Button label="C" onClick={handleClear}/>
           <Button label="/" onClick={()=>handleAddValue('/')}/>
           <Button label="*" onClick={()=>handleAddValue('*')}/>
-          <Button label="-" onClick={()=>handleAddValue('-')}/>
+          <Button label="-" onClick={handleSubNumbers}/>
         </Row>
         <Row>
           <Button label="7" onClick={()=>handleAddValue('7')}/>
           <Button label="8" onClick={()=>handleAddValue('8')}/>
           <Button label="9" onClick={()=>handleAddValue('9')}/>
-          <Button label="+" onClick={()=>handleAddValue('+')}/>
+          <Button label="+" onClick={handleSumNumbers}/>
         </Row>
         <Row>
           <Button label="4" onClick={()=>handleAddValue('4')}/>
           <Button label="5" onClick={()=>handleAddValue('5')}/>
           <Button label="6" onClick={()=>handleAddValue('6')}/>
-          <Button label="=" onClick={()=>handleAddValue('=')}/>
+          <Button label="=" onClick={handleEquals}/>
         </Row>
         <Row>
           <Button label="1" onClick={()=>handleAddValue('1')}/>
